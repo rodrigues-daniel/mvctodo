877a2c16c90926f9c699a79489aa98a908920e5e
Daniel C. Rodrigues <dc79.rodrigues@gmail.com>
2018-10-19 15:39:20 -0300

    base

---
+++ src/main/java/br/esig/todo/mvctodo/MvctodoApplication.java
@@ -0,0 +1,12 @@
+package br.esig.todo.mvctodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvctodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvctodoApplication.class, args);
+	}
+}
---
+++ src/test/java/br/esig/todo/mvctodo/MvctodoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.esig.todo.mvctodo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvctodoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
